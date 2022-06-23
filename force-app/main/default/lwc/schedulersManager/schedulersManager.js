1d012bf545811250c162ae1a1ff261c48cde76f3
aleksei.sushkevich <aleksei.sushkevich@jetbi.com>
2022-06-23 16:56:33 +0300

    fix errors

--- force-app/main/default/lwc/schedulersManager/schedulersManager.js
+++ force-app/main/default/lwc/schedulersManager/schedulersManager.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import getSchedulersData from '@salesforce/apex/SchedulersManagerController.getSchedulersData';
 import abortBatch from '@salesforce/apex/SchedulersManagerController.abortBatch';
 import scheduleBatch from '@salesforce/apex/SchedulersManagerController.scheduleBatch';
@@ -13,21 +13,26 @@ const DEFAULT_SCHEDULER = "0 0 0 * * ?";
 
 export default class SchedulersManager extends LightningElement {
 
-    @track batches = {};
+    // @track batches = {};
+    @api batchName;
+    @api schedulerName;
+    @api description;
     error;
     errorMessage;
 
-    showDropdown = false;
-    searchString = '';
-    selectedValue = '';
-    placeholder = 'Select a Batch';
+    // showDropdown = false;
+    // searchString = '';
+    // selectedValue = '';
+    // placeholder = 'Select a Batch';
     scheduleValue = '';
     tooltip = false;
-    runOnceDisabled = true;
-    scheduleBatchDisabled = true;
-    scheduleInput = true;
+    // runOnceDisabled = true;
+    // scheduleBatchDisabled = true;
+    // scheduleInput = true;
     abort = false;
-    index = 0;
+    // index = 0;
+
+    cronId;
     
 
     connectedCallback(){
@@ -35,26 +40,29 @@ export default class SchedulersManager extends LightningElement {
     }
 
     blurEvent() {
-        if(this.placeholder !== 'Select a Batch'){
-            this.searchString = this.placeholder;
-        }
-        this.showDropdown = false;
+        // if(this.placeholder !== 'Select a Batch'){
+        //     this.searchString = this.placeholder;
+        // }
+        // this.showDropdown = false;
         this.tooltip = false;
     }
 
     getAllBatches(){
-        getSchedulersData()
+        // getSchedulersData()
+        getSchedulersData({batchName : this.batchName})
         .then(data => { 
-            console.log('data ' , data);
-            this.batches = data.map(el => {
-                return{
-                    label : el.BatchClassName,
-                    value : el.BatchClassName,
-                    scheduled : el.Scheduled,
-                    cronId : el.CronId,
-                    cronExpr : el.CronExpr
-                }
-            })
+            // this.batches = data.map(el => {
+            //     return{
+            //         label : el.BatchClassName,
+            //         value : el.BatchClassName,
+            //         scheduled : el.Scheduled,
+            //         cronId : el.CronId,
+            //         cronExpr : el.CronExpr
+            //     }
+            // })
+            this.abort = data.Scheduled;
+            this.scheduleValue = data.CronExpr ? data.CronExpr : DEFAULT_SCHEDULER;
+            this.cronId = data.CronId;
         })         
         .catch(error => {
             this.error = error;
@@ -67,87 +75,88 @@ export default class SchedulersManager extends LightningElement {
                 })
             );
         }); 
-        this.scheduleValue = DEFAULT_SCHEDULER;
-        this.runOnceDisabled = true;
-        this.scheduleBatchDisabled = true;
-        this.scheduleInputDisabled = true;
-        this.placeholder = 'Select a Batch';
-        this.searchString = '';
+        // this.scheduleValue = DEFAULT_SCHEDULER;
+        // this.runOnceDisabled = true;
+        // this.scheduleInputDisabled = true;
+        // this.placeholder = 'Select a Batch';
+        // this.searchString = '';
     }
 
-    showOptions() {
-        if(this.batches){
-            this.searchString = '';
-            for(var i = 0; i < this.batches.length; i++) {
-                this.batches[i].isVisible = true;
-            }
-            if(this.batches.length > 0) {
-                this.showDropdown = true;
-            }
-        }
-    }
+    // showOptions() {
+    //     if(this.batches){
+    //         this.searchString = '';
+    //         for(var i = 0; i < this.batches.length; i++) {
+    //             this.batches[i].isVisible = true;
+    //         }
+    //         if(this.batches.length > 0) {
+    //             this.showDropdown = true;
+    //         }
+    //     }
+    // }
 
-    filterOptions(event) {
-        this.searchString = event.target.value;
-        if(this.searchString && this.searchString.length > 0) {
-            for(var i = 0; i < this.batches.length; i++) {
-                if(this.batches[i].label.toLowerCase().trim().startsWith(this.searchString.toLowerCase().trim())) {
-                    this.batches[i].isVisible = true;
-                } 
-                else {
-                    this.batches[i].isVisible = false;
-                }
-            }
-        }else{
-            for(var i = 0; i < this.batches.length; i++) {
-                this.batches[i].isVisible = true;
-            }
-        }
-    }
+    // filterOptions(event) {
+    //     this.searchString = event.target.value;
+    //     if(this.searchString && this.searchString.length > 0) {
+    //         for(var i = 0; i < this.batches.length; i++) {
+    //             if(this.batches[i].label.toLowerCase().trim().startsWith(this.searchString.toLowerCase().trim())) {
+    //                 this.batches[i].isVisible = true;
+    //             } 
+    //             else {
+    //                 this.batches[i].isVisible = false;
+    //             }
+    //         }
+    //     }else{
+    //         for(var i = 0; i < this.batches.length; i++) {
+    //             this.batches[i].isVisible = true;
+    //         }
+    //     }
+    // }
 
     scheduleChange(event){
         this.scheduleValue = event.target.value;
     }
 
-    selectItem(event) {
-        this.selectedValue = event.currentTarget.dataset.id;
-        this.searchString = event.currentTarget.textContent;
-        this.placeholder = event.currentTarget.textContent;
-        this.showDropdown = false;
-        this.scheduleBatchDisabled = false;
-        this.scheduleInputDisabled = false;
-        this.runOnceDisabled = false;
-        this.index = this.findIndex(this.selectedValue);
-        if(this.batches[this.index].cronExpr !== undefined){
-            this.abort = true;
-            this.scheduleValue = this.batches[this.index].cronExpr;
-            this.scheduleInputDisabled = true;
-        }else{
-            this.scheduleValue = DEFAULT_SCHEDULER;
-            this.abort = false;
-        }
-    }
+    // selectItem(event) {
+    //     this.selectedValue = event.currentTarget.dataset.id;
+    //     this.searchString = event.currentTarget.textContent;
+    //     this.placeholder = event.currentTarget.textContent;
+    //     this.showDropdown = false;
+    //     this.scheduleBatchDisabled = false;
+    //     this.scheduleInputDisabled = false;
+    //     this.runOnceDisabled = false;
+    //     this.index = this.findIndex(this.selectedValue);
+    //     if(this.batches[this.index].cronExpr !== undefined){
+    //         this.abort = true;
+    //         this.scheduleValue = this.batches[this.index].cronExpr;
+    //         this.scheduleInputDisabled = true;
+    //     }else{
+    //         this.scheduleValue = DEFAULT_SCHEDULER;
+    //         this.abort = false;
+    //     }
+    // }
 
-    findIndex(value){
-        let index = 0;
-        for(var i = 0; i < this.batches.length; i++){
-            if(this.batches[i].value === value){
-                index = i;
-            }
-        }
-        return index;
-    }
+    // findIndex(value){
+    //     let index = 0;
+    //     for(var i = 0; i < this.batches.length; i++){
+    //         if(this.batches[i].value === value){
+    //             index = i;
+    //         }
+    //     }
+    //     return index;
+    // }
 
     handleTooltip(){
         this.tooltip = true;
     }
 
     abortBatch(){
-        abortBatch({cronId : this.batches[this.index].cronId})
+        // abortBatch({cronId : this.batches[this.index].cronId})
+        abortBatch({cronId : this.cronId})
         .then( () =>{     
             this.dispatchEvent(
                 new ShowToastEvent({
-                    message: 'Scheduler ' + this.batches[this.index].label + ' was deleted',
+                    // message: 'Scheduler ' + this.batches[this.index].label + ' was deleted',
+                    message: 'Scheduler ' + this.batchName + ' was deleted',
                     variant: SUCCESS_VARIANT
                 })
             );
@@ -166,11 +175,13 @@ export default class SchedulersManager extends LightningElement {
     }
 
     scheduleBatch(){
-        scheduleBatch({cronExpr : this.scheduleValue, className : this.batches[this.index].label})
+        //scheduleBatch({cronExpr : this.scheduleValue, className : this.batches[this.index].label})
+        scheduleBatch({cronExpr : this.scheduleValue, className : this.batchName})
         .then( () =>{     
             this.dispatchEvent(
                 new ShowToastEvent({
-                    message: this.batches[this.index].label + ' was scheduled at ' + this.scheduleValue,
+                    // message: this.batches[this.index].label + ' was scheduled at ' + this.scheduleValue,
+                    message: this.batchName + ' was scheduled at ' + this.scheduleValue,
                     variant: SUCCESS_VARIANT
                 })
             );
@@ -189,11 +200,13 @@ export default class SchedulersManager extends LightningElement {
     }
 
     runOnce(){
-        runOnce({className : this.batches[this.index].label})
+        // runOnce({className : this.batches[this.index].label})
+        runOnce({className : this.batchName})
         .then( () =>{      
             this.dispatchEvent(
                 new ShowToastEvent({
-                    message:this.batches[this.index].label + ' was launched',
+                    // message:this.batches[this.index].label + ' was launched',
+                    message: this.batchName + ' was launched',
                     variant: SUCCESS_VARIANT
                 })
             );
